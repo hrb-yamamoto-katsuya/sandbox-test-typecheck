02304137b55b6caef18df452d8a6d97639ff9d07
hrb-yamamoto-katsuya <yamamoto.katsuya@hrbrain.co.jp>
2022-02-22 18:36:27 +0900

    init

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import * as Feature from "./index";
+
+describe("feature", () => {
+  describe("sum", () => {
+    it("should ", () => {
+      const result = Feature.sum(10, 20);
+
+      expect(result).toBe(30);
+    });
+  });
+});
