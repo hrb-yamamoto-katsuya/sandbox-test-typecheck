712287b2e6cdb8990613764403bd2ec67f920b88
hrb-yamamoto-katsuya <yamamoto.katsuya@hrbrain.co.jp>
2022-03-05 15:24:05 +0900

    feat: 実装変更

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,31 @@
 import * as Feature from "./index";
 
 describe("feature", () => {
-  describe("sum", () => {
-    it("should ", () => {
-      const result = Feature.sum(10, 20);
+  describe("getUser", () => {
+    it("when input id, then return user who matched user's id", () => {
+      const params: Parameters<typeof Feature.getUser>[0] = {
+        id: "2ECD91BB-F062-4EF5-ACA7-94808EB48150",
+        users: [
+          {
+            name: "田中太郎",
+            age: 30,
+            id: "2ECD91BB-F062-4EF5-ACA7-94808EB48150",
+          },
+          {
+            name: "田中二郎",
+            age: 20,
+            id: "0EB34E81-7F5C-4405-A98B-B67EFD3D6B2F",
+          },
+        ],
+      };
 
-      expect(result).toBe(30);
+      const result = Feature.getUser(params);
+
+      expect(result).toStrictEqual({
+        age: 30,
+        id: "2ECD91BB-F062-4EF5-ACA7-94808EB48150",
+        name: "田中太郎",
+      });
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export const sum = (a: number, b: number) => a + b;
+type User = {
+  name: string;
+  age: number;
+  id: string;
+};
+
+export const getUser = (params: { users: User[]; id: User["id"] }) =>
+  params.users.find((user) => user.id === params.id);
